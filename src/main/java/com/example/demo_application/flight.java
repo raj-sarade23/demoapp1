29afaaf16388f5e059933fd1158161741454cb8b
raj-sarade23 <elaunch.rajashri@gmail.com>
2024-07-22 17:22:55 +0530

    jenchnages

--- src/main/java/com/example/demo_application/flight.java
+++ src/main/java/com/example/demo_application/flight.java
@@ -8,7 +8,7 @@ public class flight {
     @GetMapping("/flight")
 
     public String getData() {
-        return "Please book flight in 20% discount.grab it DEV branch. testing..";
+        return "Please book flight in 20% discount.grab it DEV branch. testing-jenkins..";
     }
 
 }
