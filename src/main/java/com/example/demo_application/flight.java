1e9d69f254c49be9378aa6eb31d0478b58cdb786
raj-sarade23 <elaunch.rajashri@gmail.com>
2024-07-22 18:11:47 +0530

    test1

--- src/main/java/com/example/demo_application/flight.java
+++ src/main/java/com/example/demo_application/flight.java
@@ -8,7 +8,7 @@ public class flight {
     @GetMapping("/flight")
 
     public String getData() {
-        return "Please book flight in 20% discount.grab it DEV branch webhook testing-jenkins..";
+        return "Please book flight in 20% discount.grab it DEV branch webhook kk testing-jenkins..";
     }
 
 }
